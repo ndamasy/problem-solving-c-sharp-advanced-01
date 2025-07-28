7f209181d166fc28892446ca6e8a1880e51278d1
ndamasy <nermein4686@gmail.com>
2025-07-28 17:01:26 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace assignment_c__advanced_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+            int valueToCheck = 12;
+            bool isInRange = Range.IsInRange(numbers, valueToCheck);
+            Console.WriteLine($" Number {valueToCheck} in range? {isInRange}");
+            Console.WriteLine($"Length of the array: {Range.Length(numbers)}");
+
+        }
+    }
+}
---
+++ Range.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment_c__advanced_01
+{
+    internal class Range
+    {
+        public int max { get; set; }
+        public int min { get; set; }
+
+        public Range(int _max , int _min)
+        {
+            max = _max;
+            min = _min;
+        }
+
+        public static bool IsInRange<T>( T[] numbers , T value)
+        { 
+        
+         if(numbers is not null)
+            {
+                for(int i =0; i<numbers.Length; i++)
+                {
+                 if ( numbers[i].Equals(value))
+                    {
+                        return true;
+                    }
+                  
+                   
+                }
+            }
+            return false;
+        }
+
+
+        public static int Length<T>(T[] numbers)
+        {
+            if(numbers is not null)
+            {
+                return numbers.Length;
+            }
+            return 0;
+        }
+
+
+
+        //public override string ToString()
+        //{
+        //    return $"max :{max} \n min :{min}";
+        //}
+
+
+
+
+
+    }
+}
